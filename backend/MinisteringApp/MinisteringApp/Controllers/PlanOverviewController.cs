4a3b4b6798c006c453c0142d3b52cd49b23e6af4
lonni2020 <153124510+lonni2020@users.noreply.github.com>
2025-03-19 10:19:55 -0600

    added PlanOverviewController

---
+++ backend/MinisteringApp/MinisteringApp/Controllers/PlanOverviewController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MinisteringApp.Data;
+
+namespace MinisteringApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlanOverviewController : ControllerBase
+    {
+        private MinisteringDatabaseContext _context;
+
+        public PlanOverviewController(MinisteringDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetAllPlans")]
+
+        public IEnumerable<Plan> Get()
+        {
+            var currentPlanList = _context.Plans
+                .Where(x => x.Status == "current")
+                .ToList();
+            return currentPlanList;
+        }
+    }
+}
