141a36aac777fb0c5c9fb1b500e221f21385e1a5
annabellebbaker <annabellebaker0922@gmail.com>
2025-03-22 23:30:45 -0600

    changes to landing page to initialize dynamic features on assignments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,20 @@ import './App.css'
 import HomeScreen from './components/HomeScreen';
 import PlansSection from './components/PlansSection';
 // import NotFound from './components/NotFound'; // Create this for unknown routes
-import './App.css';
 
+// pull in the API link for localhost:5000 to connect to backend
+// vite config 3000
+// pull out components and access database
+// plans, profile, navbar, planItem, assignments, companions
+// pulling the data for the specific ID
+// fix the nav bar and hamburger functionality
 function App() {
   return (
     <>
    
     <HomeScreen />
     <PlansSection />
+    
     
     </>
   );
---
+++ frontend/src/types/assignment.ts
@@ -0,0 +1,8 @@
+export interface Assignment {
+    id: number;
+    minister: string;
+    email: string;
+    phone?: string;
+    imageUrl?: string;
+  }
+  
